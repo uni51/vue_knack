55cee757d7c695e2b0913f2a9617edc9af7e3b83
uni51 <uni51@users.noreply.github.com>
2019-12-24 23:04:17 +0900

    finish 6-5 商品一覧をSFCで再構築する

---
+++ ch06/6-5/my-product/src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app">
+    <product-list v-bind:products="products"></product-list>
+  </div>
+</template>
+
+<script>
+import productList from './components/product-list.vue'
+
+export default {
+  name: 'app',
+  components: {
+    'product-list': productList
+  },
+  data :function(){
+    return {
+      products: [
+        { id: 1, name: 'Michael<br>スマホケース', price: 1580, 
+          image: require('./assets/images/01.jpg'), delv: 0, isSale: true },
+        { id: 2, name: 'Raphael<br>スマホケース', price: 1580, 
+          image: require('./assets/images/02.jpg'), delv: 0, isSale: true },
+        { id: 3, name: 'Gabliel<br>スマホケース', price: 1580, 
+          image: require('./assets/images/03.jpg'), delv: 240, isSale: true },
+        { id: 4, name: 'Uriel<br>スマホケース', price: 980, 
+          image: require('./assets/images/04.jpg'), delv: 0, isSale: true },
+        { id: 5, name: 'Ariel<br>スマホケース', price: 980, 
+          image: require('./assets/images/05.jpg'), delv: 0, isSale: false },
+        { id: 6, name: 'Azrael<br>スマホケース', price: 1580, 
+          image: require('./assets/images/06.jpg'), delv: 0, isSale: false }
+      ]    
+    }
+  }
+}
+</script>
+
+<style>
+body {
+  /* background: #000; */
+  background: #fff;
+  /* color: #fff; */
+  color: #000;
+}
+</style>
---
+++ ch06/6-5/my-product/src/components/product.vue
@@ -0,0 +1,91 @@
+<template>
+  <div class="item">
+    <figure class="image">
+      <!-- ▼ セール対象の場合 ▼ -->
+      <template v-if="product.isSale">
+        <div class="status">SALE</div>
+      </template>
+      <!-- ▲ セール対象の場合 ▲ -->
+      <img v-bind:src="product.image" alt="">
+      <figcaption v-html="product.name"></figcaption>
+    </figure>
+    <div class="detail">
+      <div class="price"><span>{{ product.price | number_format }}</span>円（税込）</div>
+      <!-- ▼ 送料無料の場合 ▼ -->
+      <template v-if="product.delv == 0">
+        <div class="shipping-fee none">送料無料</div>
+      </template>
+      <!-- ▲ 送料無料の場合 ▲ -->
+      <!-- ▼ 送料ありの場合 ▼ -->
+      <template v-else>
+        <div class="shipping-fee">+送料{{ product.delv | number_format }}円</div>
+      </template>  
+      <!-- ▲ 送料ありの場合 ▲ -->
+    </div>
+    <!-- // end of .detail -->
+  </div>
+</template>
+
+<script>
+import './filter.js'
+
+export default {
+  name: 'Product',
+  props: ['product'] 
+}
+</script>
+
+<style scoped>
+.item {
+  flex: 0 1 250px;
+  margin-bottom: 30px;
+  border: 1px solid;
+  border-color: #000;
+}
+
+.item .image {
+  position: relative;
+  text-align: center;
+}
+
+.item .image img {
+  width: 100%;
+  height: auto;
+}
+
+.item .status {
+  position: absolute;
+  border-radius: 50%;
+  width: 4em;
+  height: 4em;
+  font-size: 12px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: #bf0000;
+  color: #fff;
+}
+
+.item .detail {
+  text-align: center;
+}
+
+.item .price {
+  display: inline-block;
+}
+
+.item .price span {
+  font-size: 180%;
+}
+
+.item .shipping-fee {
+  display: inline-block;
+  background: #f7cd12;
+  color: #000;
+}
+
+.item .shipping-fee.none {
+  background: #bf0000;
+  color: #fff;
+}
+</style>
